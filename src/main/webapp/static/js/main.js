9b3fd458a7836b2a0949cf2c2ed26cde4caaf78a
Daniel Corroto <danicq85@gmail.com>
2015-03-12 23:31:59 +0100

    Fixed #26. Cerrar sección sin partidas de la lista

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,9 +1,11 @@
 // My games
 function loadGamesList() {
 	$.getJSON("../api/games", function(data) {
+		if (data.length == 0) {
+			$("#my_games").hide();
+		}
+		$("#my_games_list").empty();
 		$.each(data, function(key, val) {
-			console.log(key);
-			console.log(val);
 			$("#my_games_list").append('<a href="game/' + val["id"] + '" class="list-group-item"><span class="badge label-success">' + val["ownPoints"] + '</span>' + val["name"] + '</a>');
 		});
 	});
@@ -12,9 +14,10 @@ function loadGamesList() {
 // Games I have been invited
 function loadInvitationList() {
 	$.getJSON("../api/invitation/list", function(data) {
+		if (data.length == 0) {
+			$("#my_invitation").hide();
+		}
 		$.each(data, function(key, val) {
-			console.log(key);
-			console.log(val);
 			$("#my_invitation_list").append('<li class="list-group-item" id="invitation_' + val["id"] + '">' + val["name"] + ' a <i>' + val["guest"] + '</i><div class="row container-separator">' +
 					'<div class="col-xs-6"><button type="button" class="btn btn-danger" onclick="cancelInvitation(' + val["id"] + ')">Cancelar</button></div>' +
 					'</div></li>');
@@ -25,9 +28,10 @@ function loadInvitationList() {
 // Games I invite
 function loadInvitedList() {
 	$.getJSON("../api/invited/list", function(data) {
+		if (data.length == 0) {
+			$("#my_invited").hide();
+		}
 		$.each(data, function(key, val) {
-			console.log(key);
-			console.log(val);
 			$("#my_invited_list").append('<li class="list-group-item" id="invited_' + val["id"] + '">' + val["name"] + ' de <i>' + val["guest"] + '</i><div class="row container-separator">' +
 					'<div class="col-xs-6"><button type="button" class="btn btn-danger" onclick="rejectInvited(' + val["id"] + ')">Rechazar</button></div>' +
 					'<div class="col-xs-6 text-right"><button type="button" class="btn btn-success" onclick="acceptInvited(' + val["id"] + ')">Aceptar</button></div>' +
@@ -40,14 +44,20 @@ function loadInvitedList() {
 function cancelInvitation(id) {
 	$.getJSON("../api/invitation/cancel/"+id, function(data) {
 		$("#invitation_"+id).remove();
+		len = $("#my_invitation_list").length;
+		if ($("#my_invitation_list").children().length == 0) {
+			$("#my_invitation").hide();
+		}
 	});
 }
 
 // Accept game I have been invited
 function acceptInvited(id) {
 	$.getJSON("../api/invited/accept/"+id, function(data) {
-		console.log(id);
 		$("#invited_"+id).remove();
+		if ($("#my_invited_list").children().length == 0) {
+			$("#my_invited").hide();
+		}
 		loadGamesList();
 	});
 }
@@ -56,6 +66,9 @@ function acceptInvited(id) {
 function rejectInvited(id) {
 	$.getJSON("../api/invited/reject/"+id, function(data) {
 		$("#invited_"+id).remove();
+		if ($("#my_invited_list").children().length == 0) {
+			$("#my_invited").hide();
+		}
 	});
 }
 
